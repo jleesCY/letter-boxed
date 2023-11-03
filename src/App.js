16d66bb05f587fe90249a7f7121213cb926b1448
jleesCY <joshualees@cedarville.edu>
2023-11-03 09:48:09 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Puzzles from "./components/Puzzles"
+import Puzzle from "./components/Puzzle"
+import NotFound from "./components/NotFound"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/puzzles" ></Navigate>}></Route>
+      <Route path="/puzzles">
+          <Route index element={<Puzzles></Puzzles>}></Route>
+          <Route path=":id" element={<Puzzle></Puzzle>}></Route>
+      </Route>
+      <Route path="*" element={<NotFound></NotFound>}></Route>
+  </Routes>
   );
 }
 
