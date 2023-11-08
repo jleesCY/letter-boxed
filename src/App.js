be0a6428970df6822d882fb68a5b6be69c4f8970
jleesCY <joshualees@cedarville.edu>
2023-11-08 08:46:31 -0500

    remove mobile block

--- src/App.js
+++ src/App.js
@@ -7,24 +7,16 @@ import NotFound from "./components/NotFound"
 import { isMobile } from 'react-device-detect'
 
 function App() {
-  if (!isMobile) {
-    return (
-      <Routes>
-        <Route path="/" element={<Navigate to="/puzzles" ></Navigate>}></Route>
-        <Route path="/puzzles">
-            <Route index element={<Puzzles></Puzzles>}></Route>
-            <Route path=":id" element={<Puzzle></Puzzle>}></Route>
-        </Route>
-        <Route path="*" element={<NotFound></NotFound>}></Route>
-      </Routes>
-    );
-  }
-  else {
-    return(
+  return (
     <Routes>
-      <Route path="*" element={<MobileLanding></MobileLanding>}></Route>
-    </Routes>);
-  }
+      <Route path="/" element={<Navigate to="/puzzles" ></Navigate>}></Route>
+      <Route path="/puzzles">
+          <Route index element={<Puzzles></Puzzles>}></Route>
+          <Route path=":id" element={<Puzzle></Puzzle>}></Route>
+      </Route>
+      <Route path="*" element={<NotFound></NotFound>}></Route>
+    </Routes>
+  );
   
 }
 
