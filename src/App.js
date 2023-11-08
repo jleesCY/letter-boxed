a8a9362c65802882410752b59bf7c06f626d05b6
jleesCY <joshualees@cedarville.edu>
2023-11-08 08:54:49 -0500

    android not working

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import Puzzles from "./components/Puzzles"
 import Puzzle from "./components/Puzzle"
 import MobileLanding from "./components/MobileLanding"
 import NotFound from "./components/NotFound"
-import { isMobile } from 'react-device-detect'
+import { isAndroid } from 'react-device-detect'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/puzzles" ></Navigate>}></Route>
-      <Route path="/puzzles">
-          <Route index element={<Puzzles></Puzzles>}></Route>
-          <Route path=":id" element={<Puzzle></Puzzle>}></Route>
-      </Route>
-      <Route path="*" element={<NotFound></NotFound>}></Route>
+  if (!isAndroid) {
+    return (
+      <Routes>
+        <Route path="/" element={<Navigate to="/puzzles" ></Navigate>}></Route>
+        <Route path="/puzzles">
+            <Route index element={<Puzzles></Puzzles>}></Route>
+            <Route path=":id" element={<Puzzle></Puzzle>}></Route>
+        </Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+    );
+  }
+  else {
+    return(
+      <Routes>
+      <Route path="*" element={<MobileLanding></MobileLanding>}></Route>
     </Routes>
-  );
+    );
+  }
   
 }
 
