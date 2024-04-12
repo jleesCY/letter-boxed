2fc280af8271129b77c63c16ebcb882deacd7639
Joshua Lees <82112905+jleesCY@users.noreply.github.com>
2024-04-11 20:06:15 -0400

    back to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Puzzles from "./components/Puzzles"
 import Puzzle from "./components/Puzzle"
 import MobileLanding from "./components/MobileLanding"
@@ -9,14 +9,14 @@ import CryptoJS from 'crypto-js';
 
 function App() {
   return (
-    <HashRouter>
+    <Routes>
       <Route path="/" element={<Navigate to="/puzzles" ></Navigate>}></Route>
       <Route path="/puzzles">
           <Route index element={<Puzzles></Puzzles>}></Route>
           <Route path=":id" element={<Puzzle></Puzzle>}></Route>
       </Route>
       <Route path="*" element={<NotFound></NotFound>}></Route>
-    </HashRouter>
+    </Routes>
   );
 }
 
